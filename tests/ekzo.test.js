49616e99f8e5537860caea529d962e7620a8b963
ArmorDarks <git@lavrins.com>
2018-02-06 03:28:21 +0200

    [tests] add testing for individuality of modules

--- tests/ekzo.test.js
+++ tests/ekzo.test.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 
 const { renderSync } = require('node-sass')
+const glob = require('glob')
 
 describe('Ekzo', () => {
   it('should compile', () => {
@@ -9,5 +10,22 @@ describe('Ekzo', () => {
     })
 
     expect(css.toString()).toMatchSnapshot()
+  })
+
+  it('modules should compile individually', (done) => {
+    glob('**/*.{scss,sass}', { ignore: '**/node_modules/**' }, (error, files) => {
+      if (error) throw new Error('Encountered an error in test')
+
+      files.forEach((file) => {
+        expect(() => {
+          try {
+            renderSync({ file })
+          } catch (error) {
+            error.message = `${file}:\n${error.message}`
+            throw new Error(error)
+          }
+        }).not.toThrow()
+      })
+    })
   })
 })
